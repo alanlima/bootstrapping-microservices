56ec6d6dc784ee28518280482dd6ee1fadadc415
Alan Lima <alima@outlook.com>
2021-04-09 09:25:26 +1000

    add azure storage account to terraform

--- infra/main.tf
+++ infra/main.tf
@@ -12,9 +12,31 @@ resource "random_string" "acr" {
 
 resource "azurerm_container_registry" "this" {
   #   name                = "bootstrappingmicroservices"
-  name                = random_string.acr.result
+  name                = "acr${var.project_name}"
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
   admin_enabled       = true
   sku                 = "Basic"
+}
+
+resource "azurerm_storage_account" "this" {
+  name                     = "st${var.project_name}"
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "videos" {
+  name = "videos"
+  storage_account_name = azurerm_storage_account.this.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "sample_video" {
+  name = "SampleVideo.mp4"
+  storage_account_name = azurerm_storage_account.this.name
+  storage_container_name = azurerm_storage_container.videos.name
+  type = "Block"
+  source = "../video-streaming/videos/SampleVideo_1280x720_1mb.mp4"
 }
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,7 +1,16 @@
 output "container_registry" {
   value = {
-    registry_url            = azurerm_container_registry.this.login_server
+    registry_url   = azurerm_container_registry.this.login_server
     admin_username = azurerm_container_registry.this.admin_username
     admin_password = azurerm_container_registry.this.admin_password
+  }
+}
+
+output "storage" {
+  value = {
+    name = azurerm_storage_account.this.name
+    blob_endpoint = azurerm_storage_account.this.primary_blob_endpoint
+    access_key = azurerm_storage_account.this.primary_access_key
+    connection_string = azurerm_storage_account.this.primary_connection_string
   }
 }
