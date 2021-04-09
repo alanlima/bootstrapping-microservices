a91dd135578a5d4604a08bb25e03a772432edec1
Alan Lima <alima@outlook.com>
2021-04-09 13:10:37 +1000

    forward video streaming to azure storage service

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,34 +1,48 @@
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
+const http = require("http");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
+const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
 
-
-/**
- * Registers a HTTP get route for video streamming
- */
+// Registers a HTTP get route for video streamming
 app.get("/video", (req, res) => {
-  const videoPath = path.join('./videos', 'SampleVideo_1280x720_1mb.mp4');
-  fs.stat(videoPath, (err, stats) => {
-    if(err) {
-      console.error('An error occurred', err);
-      res.sendStatus(500);
-      return;
-    }
+  const forwardRequest = http.request({
+    host: VIDEO_STORAGE_HOST,
+    port: VIDEO_STORAGE_PORT,
+    path: `/video?path=SampleVideo.mp4`,
+    method: 'GET',
+    headers: req.headers,
+  }, forwardResponse => {
+    res.writeHead(
+      forwardResponse.statusCode,
+      forwardResponse.headers
+    );
+    forwardResponse.pipe(res);
+  });
 
-    res.writeHead(200, {
-      'Content-Length': stats.size,
-      'Content-Type': 'video/mp4'
-    });
-    fs.createReadStream(videoPath).pipe(res);
-  })
+  req.pipe(forwardRequest);
+
+  // const videoPath = path.join('./videos', 'SampleVideo_1280x720_1mb.mp4');
+  // fs.stat(videoPath, (err, stats) => {
+  //   if(err) {
+  //     console.error('An error occurred', err);
+  //     res.sendStatus(500);
+  //     return;
+  //   }
+
+  //   res.writeHead(200, {
+  //     'Content-Length': stats.size,
+  //     'Content-Type': 'video/mp4'
+  //   });
+  //   fs.createReadStream(videoPath).pipe(res);
+  // })
 });
 
 /**
  * Starts the HTTP server
  */
-app.listen(port, () => {
-  console.log(`Microservice listening on port ${port}, point your browser at localhost:${port}/video`);
+app.listen(PORT, () => {
+  console.log(`Microservice listening on port ${PORT}, point your browser at localhost:${PORT}/video`);
 })
