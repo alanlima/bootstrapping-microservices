43c85f9d77c2951d1231e6e7a3c8b7898f969c02
Alan Lima <alima@outlook.com>
2021-04-09 19:59:48 +1000

    add mongodb service; update video-streaming to connect with mongodb

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,48 +1,65 @@
 const express = require("express");
 const http = require("http");
+const mongodb = require('mongodb');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
 const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+const DBHOST = process.env.DBHOST;
+const DBNAME = process.env.DBNAME;
 
-// Registers a HTTP get route for video streamming
-app.get("/video", (req, res) => {
-  const forwardRequest = http.request({
-    host: VIDEO_STORAGE_HOST,
-    port: VIDEO_STORAGE_PORT,
-    path: `/video?path=SampleVideo.mp4`,
-    method: 'GET',
-    headers: req.headers,
-  }, forwardResponse => {
-    res.writeHead(
-      forwardResponse.statusCode,
-      forwardResponse.headers
-    );
-    forwardResponse.pipe(res);
+async function main () {
+  const client = await mongodb.MongoClient.connect(DBHOST);
+  const db = client.db(DBNAME);
+  const videosCollection = db.collection("videos");
+  // Registers a HTTP get route for video streamming
+  app.get("/video", (req_1, res) => {
+
+    const videoId = new mongodb.ObjectId(req_1.query.id);
+    videosCollection
+      .findOne({ _id: videoId })
+      .then(videoRecord => {
+        if (!videoRecord) {
+          res.sendStatus(404);
+          return;
+        }
+
+        const forwardRequest = http.request({
+          host: VIDEO_STORAGE_HOST,
+          port: VIDEO_STORAGE_PORT,
+          path: `/video?path=${videoRecord.videoPath}`,
+          method: 'GET',
+          headers: req_1.headers,
+        }, forwardResponse => {
+          res.writeHead(
+            forwardResponse.statusCode,
+            forwardResponse.headers
+          );
+          forwardResponse.pipe(res);
+        });
+
+        req_1.pipe(forwardRequest);
+      })
+      .catch(err => {
+        console.error("Database query failed.");
+        console.error(err && err.stack || err);
+        res.sendStatus(500);
+      });
   });
+  /**
+   * Starts the HTTP server
+   */
+  app.listen(PORT, () => {
+    console.log(`Microservice listening on port ${PORT}, point your browser at localhost:${PORT}/video`);
+  });
+}
 
-  req.pipe(forwardRequest);
-
-  // const videoPath = path.join('./videos', 'SampleVideo_1280x720_1mb.mp4');
-  // fs.stat(videoPath, (err, stats) => {
-  //   if(err) {
-  //     console.error('An error occurred', err);
-  //     res.sendStatus(500);
-  //     return;
-  //   }
-
-  //   res.writeHead(200, {
-  //     'Content-Length': stats.size,
-  //     'Content-Type': 'video/mp4'
-  //   });
-  //   fs.createReadStream(videoPath).pipe(res);
-  // })
-});
-
-/**
- * Starts the HTTP server
- */
-app.listen(PORT, () => {
-  console.log(`Microservice listening on port ${PORT}, point your browser at localhost:${PORT}/video`);
-})
+main()
+  .then(() => {
+    console.log("Microservice online")
+  })
+  .catch(err => {
+    console.error('Microservice failed to start');
+    console.error(err && err.stack || err);
+  });
